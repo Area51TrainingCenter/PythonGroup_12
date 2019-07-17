a8258af366fc6d036e01b881946141b1c3f58075
Kevyn franco clada <kevyn8@hotmail.com>
2019-07-17 18:48:35 -0500

    Clase 17 julio

---
+++ Clase 17 julio/Skeleton_Django/settings/settings_staging.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'zdacmjmq',
+        'USER': 'zdacmjmq',
+        'PASSWORD': 'efd3biudG2vRrS-0o0nkfV8QDniDJnVh',
+        'HOST': 'raja.db.elephantsql.com',
+        'PORT': '',
+    },
+}
+
+DEBUG = True
+BASE_URL = 'http://localhost:8000'
